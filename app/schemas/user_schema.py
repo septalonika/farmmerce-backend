fc5352633dd5522cc0e581dc0a340117af11818a
septalonika <andikabeatbox@gmail.com>
2025-05-03 01:13:39 +0700

    update: modify user_schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,18 +1,20 @@
-from pydantic import BaseModel
-
-
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from enum import Enum
 class User(BaseModel):
     id: int
     email: str
-    hashed_password: str
-    
+    password: str
 
+
+class GenderEnum(str, Enum):
+    Male = "Male"
+    Female = "Female"
 
 class UserCreate(BaseModel):
-    email: str
-    hashed_password: str
-
-
-class UserUpdate(BaseModel):
-    email: str | None = None
-    hashed_password: str | None = None
+    email: EmailStr
+    username: str
+    password: str
+    first_name: str
+    last_name: str
+    gender: Optional[GenderEnum]
