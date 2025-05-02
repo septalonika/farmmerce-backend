f060aaa228440292a6417a76afe5f7f4f3fd5186
septalonika <andikabeatbox@gmail.com>
2025-05-02 19:25:57 +0700

    update: app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,21 @@ from fastapi import FastAPI
 from app.routers.user import user_router
 from app.settings import settings
 from contextlib import asynccontextmanager
+from app.views.user import UserView
+import os
+from dotenv import load_dotenv
 
-async def startup():
-    await database.connect()
+# Load .env file - this should be at the very beginning of your application
+load_dotenv(override=True)
 
-async def shutdown():
-    await database.disconnect()
 
 def create_app():
 
-    @asynccontextmanager
-    async def lifespan(app:FastAPI):
-        await startup()
-        yield
-        await shutdown()
-
-    app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
+    print(f"DATABASE_URL: {os.getenv('DATABASE_URL')}")
+    app = FastAPI()
+    @app.get("/")
+    def read_root():
+        return {"Message": "This is Index of Farmmerce API"}
 
     app.include_router(user_router)
 
