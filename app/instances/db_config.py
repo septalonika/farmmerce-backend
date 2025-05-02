863ae8670acf56667219c38944db225183cbc056
septalonika <andikabeatbox@gmail.com>
2025-05-02 19:26:42 +0700

    update: using dotenv to load env

--- app/instances/db_config.py
+++ app/instances/db_config.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from databases import Database
 from app.settings import settings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 engine = create_engine(settings.DATABASE_URL)
 database = Database(settings.DATABASE_URL)
