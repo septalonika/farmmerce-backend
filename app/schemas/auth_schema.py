f7d5a6293ef10bc6db29b1bb4d4d3b457ffdb726
septalonika <andikabeatbox@gmail.com>
2025-05-06 13:04:17 +0700

    update: add register schema

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -4,3 +4,16 @@ from typing import Optional
 class Login(BaseModel):
     email: str
     password: str
+
+class Register(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+    first_name: str
+    last_name: str
+    gender: Optional[str]
+    address: Optional[str]
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
