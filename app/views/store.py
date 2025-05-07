f8fd120ff62f03b6c4d57e92eb8dffb7bb375034
septalonika <andikabeatbox@gmail.com>
2025-05-08 02:17:11 +0700

    update: add get_all_stores

--- app/views/store.py
+++ app/views/store.py
@@ -11,5 +11,34 @@ class StoreView():
         self.store_repo = StoreRepository()
     
     def get_all_stores(self, db: Session):
-        pass
+        try:
+            response = self.store_repo.get_all(db)
+            if response is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={
+                        "success": True,
+                        "message": "No users found",
+                        "status": 404
+                    }
+                )
+            else:
+                serialized_response = [user.serialize() for user in response]
+                return JSONResponse(
+                    status_code=status.HTTP_200_OK,
+                    content={
+                        "success": True,
+                        "data": serialized_response,
+                        "status": 200
+                    }
+                )
+        except Exception as e:
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "success": False,
+                    "message": str(e),
+                    "status": 500
+                }
+            )
             
