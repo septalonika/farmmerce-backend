de67278fcdf792fd15d906de541f9c8f00ea559c
septalonika <andikabeatbox@gmail.com>
2025-05-07 14:50:27 +0700

    update: using Settings for extract JWT things

--- app/views/auth.py
+++ app/views/auth.py
@@ -7,15 +7,11 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta, timezone
+from app.settings import settings
 import jwt
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 class Token(BaseModel):
     access_token: str
@@ -34,12 +30,25 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
+class UsernameInDB:{
+    "username": str
+}
+    
+class UserInDB():
+    hashed_password: str
+    username: str
+
+class Payload():
+    email: str
+    password: str
+
+
 class AuthView():
     def __init__(self):
         self.user_repo = UserRepository()
     
-    def authenticate_user(self, email: str, password: str, db):
-        user = self.user_repo.get_credential(email, db)
+    def authenticate_user(self, email: str, password: str, db) -> UsernameInDB | None:
+        user : UserInDB = self.user_repo.get_credential(email, db)
         if not user:
             return None
         if not user.check_password(password):
@@ -55,10 +64,10 @@ class AuthView():
         else:
             expire = datetime.now(timezone.utc) + timedelta(minutes=15)
         to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
         return encoded_jwt
 
-    def login(self, payload, db: Session):
+    def login(self, payload: Payload, db: Session):
         user = self.authenticate_user(payload.email, payload.password, db)
         if user is None:
             raise HTTPException(
@@ -66,11 +75,22 @@ class AuthView():
                 detail="Incorrect username or password",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token_expires = timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
+        refresh_token_expires = timedelta(days=settings.JWT_REFRESH_TOKEN_EXPIRE_DAYS)
         access_token = self.create_access_token(
             data={"sub": user['username']}, expires_delta=access_token_expires
         )
-        return Token(access_token=access_token, token_type="bearer")
+        refresh_token = self.create_access_token(
+            data={"sub": user['username']}, expires_delta=refresh_token_expires
+        )
+        return JSONResponse({
+            "success": True,
+            "data": {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "token_type": "bearer"
+            }
+        })
     
     def get_me(self, credential, db: Session):
         return self.user_repo.get_user_by(credential, db)
