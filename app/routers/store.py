524d7c58ee7833b36931bb72d6e937363fe2609a
septalonika <andikabeatbox@gmail.com>
2025-05-08 02:00:22 +0700

    init: store routers

---
+++ app/routers/store.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends
+from app.views.user import UserView
+from app.views.store import StoreView
+from sqlalchemy.orm import Session
+from app.instances.db_config import get_db
+
+store_router = APIRouter(prefix="/api/v1/stores", tags=["users"])
+user_view = UserView()
+store_view = StoreView()
+
+@store_router.get("")
+async def get_all_stores(db: Session = Depends(get_db)):
+    return store_view.get_all_stores(db)
+
+@store_router.post("")
+async def create_store(payload, db: Session = Depends(get_db)):
+    pass
+
+@store_router.put("/{id}")
+async def update_store(id, payload, db: Session = Depends(get_db)):
+    pass
+
+@store_router.delete("/{id}")
+async def delete_store(id, db: Session = Depends(get_db)):
+    pass
+
+@store_router.get("/{id}")
+async def get_store(id, db: Session = Depends(get_db)):
+    pass
+
