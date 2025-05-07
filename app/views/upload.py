f6321580345e3c60d861b40c4210b9b44182402c
septalonika <andikabeatbox@gmail.com>
2025-05-08 01:58:29 +0700

    update: change naming for upload_user_avatar, init upload_store_avatar and add_product_image

--- app/views/upload.py
+++ app/views/upload.py
@@ -8,7 +8,7 @@ class UploadView():
     def __init__(self):
         self.user_repo = UserRepository()
     
-    def upload_file(self, id, url, db):
+    def upload_user_avatar(self, id, url, db):
         try:
             if(UserView().find_user(id, db) is None):
                 return JSONResponse(
@@ -47,3 +47,82 @@ class UploadView():
                     "status": 500
                 }
             )
+    def upload_store_avatar(self, id, url, db):
+        try:
+            if(UserView().find_user(id, db) is None):
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={
+                        "success": False,
+                        "message": "User not found",
+                        "status": 404
+                    }
+                )
+            response = self.user_repo.update_avatar(id, url, db)
+            if(response['success'] == False):
+                return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                        "success": False,
+                        "message": response['message'],
+                        "status": 500
+                    }
+                )
+            return JSONResponse(
+                    status_code=status.HTTP_200_OK,
+                    content={
+                        "success": True,
+                        "data": response,
+                        "message": response['message'],
+                        "status": 200
+                    }
+                )
+        except Exception as e:
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "success": False,
+                    "message": str(e),
+                    "status": 500
+                }
+            )
+    def add_product_image(self, id, url, db):
+        try:
+            if(UserView().find_user(id, db) is None):
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={
+                        "success": False,
+                        "message": "User not found",
+                        "status": 404
+                    }
+                )
+            response = self.user_repo.update_avatar(id, url, db)
+            if(response['success'] == False):
+                return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                        "success": False,
+                        "message": response['message'],
+                        "status": 500
+                    }
+                )
+            return JSONResponse(
+                    status_code=status.HTTP_200_OK,
+                    content={
+                        "success": True,
+                        "data": response,
+                        "message": response['message'],
+                        "status": 200
+                    }
+                )
+        except Exception as e:
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "success": False,
+                    "message": str(e),
+                    "status": 500
+                }
+            )
+            
