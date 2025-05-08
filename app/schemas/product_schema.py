ccc8ea9d126ce3916334f18df66fd43f2bf7665d
septalonika <andikabeatbox@gmail.com>
2025-05-08 08:44:23 +0700

    init: create product schema

---
+++ app/schemas/product_schema.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    stock: int
+    rating: int
+    image: str
+    store_id: int
+    created_at: str
+    updated_at: str
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str
+    price: float
+    stock: int
+    rating: int
+    image: str
+
+class ProductUpdate(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[float]
+    stock: Optional[int]
+    rating: Optional[int]
+    image: Optional[str]
+    
