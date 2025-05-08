3141a3442b111a931068dd3eb026559118ef3854
septalonika <andikabeatbox@gmail.com>
2025-05-08 11:42:09 +0700

    fix: update settings

--- app/settings.py
+++ app/settings.py
@@ -1,16 +1,17 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 class Settings(BaseSettings):
     DATABASE_URL: str
     APP_NAME: str = "DefaultApp" 
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
-    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
-    AWS_S3_BUCKET_NAME: str
-    AWS_REGION_NAME: str
-    AWS_ENDPOINT_URL: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    AWS_ACCESS_KEY_ID: Optional[str] = None
+    AWS_SECRET_ACCESS_KEY: Optional[str] = None
+    AWS_S3_BUCKET_NAME: Optional[str] = None
+    AWS_REGION_NAME: Optional[str] = None
+    AWS_ENDPOINT_URL: Optional[str] = None
 
     class Config:
         env_file = ".env"
