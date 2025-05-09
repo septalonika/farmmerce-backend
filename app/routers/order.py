d0a95972b06f8db784010471ba4d45115a9b3204
septalonika <andikabeatbox@gmail.com>
2025-05-09 10:21:01 +0700

    add estimate and province search

--- app/routers/order.py
+++ app/routers/order.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from app.views.order import OrderView
-from app.schemas.order_schema import OrderCreate, OrderUpdate
+from app.schemas.order_schema import OrderCreate, OrderUpdate,OrderEstimate
 from sqlalchemy.orm import Session
 from app.instances.db_config import get_db
+from app.middleware.s3.rajaOngkir import get_province
 
 order_router = APIRouter(prefix="/api/v1/orders", tags=["orders"])
 order_view = OrderView()
@@ -10,6 +11,10 @@ order_view = OrderView()
 @order_router.get("")
 async def get_all_orders(db: Session = Depends(get_db)):
     return order_view.get_all_orders(db)
+
+@order_router.get("/province")
+async def get_province_route(keywords: str = Query(...)):
+    return order_view.get_province(keywords)
 
 @order_router.get("/{id}")
 async def get_order(id, db: Session = Depends(get_db)):
@@ -22,3 +27,7 @@ async def create_order(payload: OrderCreate, db: Session = Depends(get_db)):
 @order_router.put("/{id}")
 async def update_order_status(id, payload: OrderUpdate, db: Session = Depends(get_db)):
     return order_view.update_order_status(id, payload, db)
+
+@order_router.post("/estimate")
+async def get_province(payload: OrderEstimate):
+    return payload
